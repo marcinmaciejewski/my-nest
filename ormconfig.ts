2f90f31f3c3ec96aa82ae3a2e690b8ac8fb60d4f
Marcin Maciejewski <marcin.maciejewski@inspeerity.com>
2022-09-07 16:18:05 +0200

    Use env variables in ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,15 +1,17 @@
+require('dotenv').config();
+
 import {PostgresConnectionOptions} from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 const config: PostgresConnectionOptions = {
   name: "default",
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "admin",
-  password: "admin",
-  database: "noft",
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   entities: ["dist/**/*.entity{ .ts,.js}"],
-  synchronize: false,
+  synchronize: process.env.SYNCHRONIZE === 'true',
   migrations: ["dist/migrations/*{.ts,.js}"],
   migrationsTableName: "migrations_typeorm",
   migrationsRun: true
